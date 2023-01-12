9a0c83ce179d55ae1c5e452425ea3362ae95ce10
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2023-01-12 15:16:24 -0300

    fix: temporarily remove faviconn plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
+// import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     NgmiPolyfill(),
-    vitePluginFaviconsInject("./src/assets/asteroid.svg"),
+    // vitePluginFaviconsInject("./src/assets/asteroid.svg"),
     react(),
   ],
   define: { global: "window" },
