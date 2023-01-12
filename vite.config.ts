fa4ab76aa86d65e5aeeb2c2562ff075f265d901f
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2023-01-12 15:06:58 -0300

    chore: update asteroidkit and add polifylls

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
+import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vitePluginFaviconsInject("./src/assets/asteroid.svg"), react()],
+  plugins: [
+    vitePluginFaviconsInject("./src/assets/asteroid.svg"),
+    NgmiPolyfill(),
+    react(),
+  ],
+  define: { global: "window" },
 });
